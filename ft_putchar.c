59dc84ec65bf9832044d2add6a279719b872daa0
Helene BRULIN <hbrulin@e2r6p1.42.fr>
2019-12-08 11:01:17 +0100

    norme

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,14 +6,14 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 10:46:34 by hbrulin           #+#    #+#             */
-/*   Updated: 2019/11/21 11:00:58 by hbrulin          ###   ########.fr       */
+/*   Updated: 2019/12/08 10:59:33 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int     ft_putchar(char c)
+int		ft_putchar(char c)
 {
-    write(1, &c, 1);
-    return(0);
+	write(1, &c, 1);
+	return (0);
 }
