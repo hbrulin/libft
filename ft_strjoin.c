9a98010ed9c297723f24cc29caf0b3371d4665c8
Helene BRULIN <hbrulin@e2r7p10.42.fr>
2020-01-22 18:10:01 +0100

    norme

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,18 +6,23 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 19:43:58 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/01/22 18:04:48 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/01/22 18:09:42 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strjoin(char const *s1, char const *s2)
+char		*ft_strjoin(char const *s1, char const *s2)
 {
-	int i = 0;
-	int j = 0;
-	int len = ft_strlen(s1) + ft_strlen(s2);
-	char *dst = ft_strnew(len);
+	int		i;
+	int		j;
+	int		len;
+	char	*dst;
+
+	i = 0;
+	j = 0;
+	len = ft_strlen(s1) + ft_strlen(s2);
+	dst = ft_strnew(len);
 	while (s1[i])
 	{
 		dst[j] = s1[i];
