8a10ce0c6c609f8230c6b7bcbf6b9b9e8fa5402f
Helene BRULIN <hbrulin@e2r7p14.42.fr>
2020-02-06 11:24:18 +0100

    ft_strcmp, ft_tablen, ft_copy_tab, ft_is_space, ft_lsttotab

---
+++ ft_copy_tab.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_copy_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/06 11:16:11 by hbrulin           #+#    #+#             */
+/*   Updated: 2020/02/06 11:16:35 by hbrulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char **copy_tab(char **tab)
+{
+	char **cpy;
+	char **cpy_ret;
+	size_t len;
+
+	if (!(len = ft_tablen(tab)))
+		return (NULL);
+	if (!(cpy = (char**)malloc(sizeof(char*) * (len + 1))))
+		return (NULL);
+	cpy_ret = cpy;
+	cpy[len] = NULL;
+	while (len--)
+		*cpy++ = ft_strdup(*tab++);
+	return(cpy_ret);
+}
---
+++ ft_lst_to_tab.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lst_to_tab.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/06 11:17:41 by hbrulin           #+#    #+#             */
+/*   Updated: 2020/02/06 11:18:17 by hbrulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char **ft_lst_to_tab(t_list *list)
+{
+	char **ret;
+	t_list	*tmp;
+	int i;
+
+	if (!(ret = (char**)malloc(sizeof(char *) * ft_lstsize(list) + 1)))
+		return (NULL);
+	i = 0;
+	tmp = list;
+	while (tmp)
+	{
+		if (!(ret[i] = ft_strdup(tmp->content)))
+			return (NULL);
+		tmp = tmp->next;
+		i++;
+	}
+	ret[i] = 0;
+	return (ret);
+}
