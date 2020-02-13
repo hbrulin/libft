85ef2cfcd577be14ef115373c11e0f4f454fc80f
Helene BRULIN <hbrulin@e2r4p11.42.fr>
2020-02-13 13:44:34 +0100

    fix leak search_n_destroy, norme ft_list, sub_tab, lst_uter_cmp et n_cmp

--- ft_tab_to_list.c
+++ ft_tab_to_list.c
@@ -6,27 +6,28 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/09 17:17:33 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/02/09 17:28:19 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/02/13 13:41:50 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_tab_to_list(t_list *lst, char **tab)
+t_list	*ft_tab_to_list(t_list **lst, char **tab)
 {
-	t_list *temp = NULL;
-	int i;
+	t_list	*temp;
+	int		i;
 
 	i = 0;
-	while(tab[i])
+	temp = NULL;
+	while (tab[i])
 	{
 		if (!(temp = malloc(sizeof(t_list))))
 			return (NULL);
 		if (!(temp->content = ft_strdup(tab[i])))
 			return (NULL);
 		temp->next = 0;
-		ft_lstadd_back(&lst, temp);
+		ft_lstadd_back(lst, temp);
 		i++;
 	}
-	return(lst);
+	return (*lst);
 }
